e0ea3795d89f0476b65148f4c22ea362fc9bfd6e
נתנאל מיגלוביץ <natanel2@office.eduil.org>
2023-03-25 11:02:49 +0300

    gallery upload works (unstable tho) -- using activity as a dialog now (works well)
    -- worked on transitioning from startActivity for result
    
    -- no more startActivityForResult
    
    need to fix cameraIntent and clean code

--- app/src/main/java/com/example/tribu_inital/Sign_up_page.java
+++ app/src/main/java/com/example/tribu_inital/Sign_up_page.java
@@ -7,8 +7,10 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -56,12 +58,14 @@ public class Sign_up_page extends AppCompatActivity implements View.OnClickListe
 
     String picName;
 
+    ActivityResultLauncher<Intent> mStartDialog;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_up_page);
         name=findViewById(R.id.NameInput);
         email=findViewById(R.id.emailInput);
         pass=findViewById(R.id.passwordText);
@@ -83,17 +87,21 @@ public class Sign_up_page extends AppCompatActivity implements View.OnClickListe
 
 
 
-       submit.setOnClickListener(this);
+        submit.setOnClickListener(this);
 
 
-//       ActivityResultLauncher<String> mStartDialog = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-//               new ActivityResultCallback<ActivityResult>() {
-//                   @Override
-//                   public void onActivityResult(ActivityResult result) {
-//
-//                   }
-//               }
-//       );
+        mStartDialog = registerForActivityResult(
+               new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() !=-1) return;
+                    Intent data = result.getData();
+                    assert data != null;
+                    uri = data.getData();
+                    picName = System.currentTimeMillis()+".image";
+                    continueCreatingUser();
+
+                }
+       );
     }
 
     public void createUser(){
@@ -105,8 +113,12 @@ public class Sign_up_page extends AppCompatActivity implements View.OnClickListe
             //default user photo
             uri = Uri.parse("android.resource://com.example.tribu_inital/"+R.mipmap.user);
 
+            requestPermissions(new String[] {Manifest.permission.CAMERA},1);
+
+
             intent = new Intent(Sign_up_page.this, User_photo_dialog.class);
-            startActivityForResult(intent,1);
+            mStartDialog.launch(intent);
+            //startActivityForResult(intent,1);
 
         }
 
@@ -135,11 +147,11 @@ public class Sign_up_page extends AppCompatActivity implements View.OnClickListe
                         ref.child(user.getKey()).setValue(user);
                         progressBar.setVisibility(View.GONE);
 
-                        mStorageRef= FirebaseStorage.getInstance().getReference("Images/Users/"+user.getKey());
+                        mStorageRef = FirebaseStorage.getInstance().getReference("Images/Users/"+ user.getEmail().replace('.',' '));
                         mStorageRef = mStorageRef.child(picName);
                         mStorageRef.putFile(uri).addOnSuccessListener(taskSnapshot ->
                                         Toast.makeText(this,"user photo uploaded Successfully!",Toast.LENGTH_SHORT).show())
-                                .addOnFailureListener(e -> Toast.makeText(this,""+e,Toast.LENGTH_LONG).show());
+                                        .addOnFailureListener(e -> Toast.makeText(this,""+e,Toast.LENGTH_LONG).show());
 
                         Toast toast=Toast.makeText(getApplicationContext(),"Account created!",Toast.LENGTH_SHORT);
                         toast.show();
--- app/src/main/java/com/example/tribu_inital/User_photo_dialog.java
+++ app/src/main/java/com/example/tribu_inital/User_photo_dialog.java
@@ -1,9 +1,15 @@
 package com.example.tribu_inital;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.provider.SyncStateContract;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -13,17 +19,25 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ThemedSpinnerAdapter;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.net.URI;
 
 public class User_photo_dialog extends AppCompatActivity implements View.OnClickListener {
 
 
     Button cameraButton, galleryButton, skipButton;
 
-    ActivityResultLauncher<String> mTakePhoto;
+    ActivityResultLauncher mTakePhoto;
+
+    ActivityResultLauncher<Intent> mPhotoFromGallery;
 
     Intent intent;
 
-    //Uri uri;
+    Uri uri;
 
 
 
@@ -42,15 +56,39 @@ public class User_photo_dialog extends AppCompatActivity implements View.OnClick
         galleryButton.setOnClickListener(this);
         skipButton.setOnClickListener(this);
 
+
         mTakePhoto = registerForActivityResult(
-                new ActivityResultContracts.GetContent(),
+                new ActivityResultContracts.TakePicture(),
                 result -> {
+                    if(!result) finish();
                     intent = new Intent();
-                    intent.putExtra("uri",result);
-                    setResult(RESULT_OK,intent);
-                    this.finish();
+                    intent.setData(uri);
+                    finish();
+
+
+
                 }
+
         );
+
+        mPhotoFromGallery = registerForActivityResult(
+          new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() != -1) return;
+                    intent = new Intent();
+                    Intent data = result.getData();
+                    assert data != null;
+                    intent.setData(data.getData());
+                    getBaseContext().getContentResolver().takePersistableUriPermission(data.getData(), Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                    setResult(RESULT_OK,intent);
+                    finish();
+                    }
+        );
+
+//
+//        if (ActivityCompat.checkSelfPermission(User_photo_dialog.this,
+//                Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
+//            finish();
 
     }
     @Override
@@ -58,12 +96,16 @@ public class User_photo_dialog extends AppCompatActivity implements View.OnClick
         if(view == skipButton) finish();
 
         if(view == galleryButton){
-            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, 1);
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.setType("image/*");
+            intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            mPhotoFromGallery.launch(intent);
+            //mPhotoFromGallery.launch("image/*");
         }
 
         if(view == cameraButton){
-            mTakePhoto.launch("image/*");
+
+          mTakePhoto.launch(uri);
 
         }
 
