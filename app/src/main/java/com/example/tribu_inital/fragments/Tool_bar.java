5a0bf2f17d8e5b3ee0992151e3ff4fe4e229e4b1
נתנאל מיגלוביץ <natanel2@office.eduil.org>
2023-05-02 23:10:48 +0300

    fixed the code
    changed the top app bar
    
    added the fragments to Tool_bar
    
    added chat_page_fragment.java
    
    and explore_page_fragment.java
    
    not sure if it works well

--- app/src/main/java/com/example/tribu_inital/fragments/Tool_bar.java
+++ app/src/main/java/com/example/tribu_inital/fragments/Tool_bar.java
@@ -3,6 +3,7 @@ package com.example.tribu_inital.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -41,43 +42,40 @@ public class Tool_bar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_tool_bar, container, false);
+        View view = inflater.inflate(R.layout.fragment_tool_bar, container, false);
         bottomNavigation = view.findViewById(R.id.buttom_tool_bar);
 
-        FragmentManager fragmentManager=getChildFragmentManager();
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        fragmentTransaction.replace(R.id.content, new home_fragment()).commit();
-
-
-        if(savedInstanceState == null){
-            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
-            ft.add(R.id.fragment_container, new home_fragment()).commit();
-        }
+        fragmentTransaction.replace
+                (R.id.fragment_container, new home_fragment()).commit();
 
 
         bottomNavigation.setOnItemSelectedListener(item -> {
-
+            fragmentTransaction.addToBackStack(null);
+            FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
             switch (item.getItemId())
             {
-                // Todo: add all the page fragments
-
                 case R.id.explore_page_icon:{
-
+                    fragmentTransaction1.replace
+                            (R.id.fragment_container, new explore_page_fragment()).commit();
+                    return true;
                 }
                 case R.id.chat_page_icon:{
-
+                    fragmentTransaction1.replace
+                            (R.id.fragment_container, new chat_page_fragment()).commit();
+                    return true;
                 }
                 case R.id.Home_page_icon:{
-
+                    fragmentTransaction1.replace
+                            (R.id.fragment_container, new home_fragment()).commit();
+                    return true;
                 }
             }
 
-
-            return true;
+            return false;
         });
-
-        // Inflate the layout for this fragment
 
         return view;
     }
